beb5e8fe30ebc20c5efc29d71999313b4ffb72b1
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-03-19 18:23:14 +0000

    update app

--- app/app.R
+++ app/app.R
@@ -7,7 +7,7 @@ library(shinythemes)
 library(shinyWidgets)
 
 # Data
-load(url("https://raw.githubusercontent.com/nrennie/30daychartchallenge/main/data/all_data.RData"))
+load(url("https://raw.githubusercontent.com/nrennie/30DayChartChallenge/main/data/all_data.RData"))
 all_years <- unique(all_data$year)
 
 # Define UI for app that draws a histogram ----
@@ -24,7 +24,7 @@ ui <- fluidPage(
 
 The 30 Day Chart Challenge is an annual data visualization challenge that takes place throughout April. Each day, participants create and share a chart based on a given daily prompt. The challenge encourages creativity, data storytelling, and improving data visualisation skills. It typically follows a structured theme for each week, such as comparisons, distributions, or time series. People usually share their work on social media using the hashtag #30DayChartChallenge.
 
-My contributions can be found on [GitHub](https://github.com/nrennie/30daychartchallenge), and you can use this Shiny app to explore my visualisations with links to code for each individual plot. You can also follow my attempts on [Mastodon](https://fosstodon.org/@nrennie) and [BlueSky](https://bsky.app/profile/nrennie.bsky.social).
+My contributions can be found on [GitHub](https://github.com/nrennie/30DayChartChallenge), and you can use this Shiny app to explore my visualisations with links to code for each individual plot. You can also follow my attempts on [Mastodon](https://fosstodon.org/@nrennie) and [BlueSky](https://bsky.app/profile/nrennie.bsky.social).
 "),
 htmltools::hr(),
 shinyWidgets::pickerInput(
@@ -34,9 +34,11 @@ shinyWidgets::pickerInput(
 ),
 # choose a plot
 shiny::uiOutput("select_img"),
+shiny::textOutput("tools_used"),
+htmltools::br(),
 # display information
-# shiny::htmlOutput("code_link"),
-# htmltools::br(),
+shiny::htmlOutput("code_link"),
+htmltools::br(),
 width = 6
     ),
 
@@ -69,30 +71,54 @@ server <- function(input, output) {
   # Get data
   plot_data <- reactive({
     req(input$plot_title)
-    dplyr::filter(all_weeks, title == input$plot_title)
+    dplyr::filter(all_data, year == input$year_select, prompt == input$plot_title)
   })
 
   ## Image display
   img_path <- shiny::reactive({
-    glue::glue("https://raw.githubusercontent.com/nrennie/30daychartchallenge/main/{plot_data()$img_url}")
+    glue::glue("{plot_data()$image_url}")
   })
 
   output$plot_img <- shiny::renderText({
     c('<img src="', img_path(), '" width="100%">')
   })
 
-  ### Code link
-  # code_path <- shiny::reactive({
-  #   glue::glue(
-  #     "https://github.com/nrennie/tidytuesday/tree/main/{week_data()$code_fpath}"
-  #   )
-  # })
+  ## Tools used
+  output$tools_used <- shiny::renderText({
+    glue::glue(
+      "This plot was made with {plot_data()$tool}."
+    )
+  })
 
-  # output$code_link <- shiny::renderText({
-  #   glue::glue(
-  #     '<b>Code is available at</b>: <a href="{code_path()}"  target="_blank">{code_path()}</a>.'
-  #   )
-  # })
+  ### Code link
+  code_path <- shiny::reactive({
+    if (plot_data()$tool == "R") {
+      glue::glue(
+        "https://github.com/nrennie/30DayChartChallenge/tree/main/{plot_data()$year}/scripts/day_{plot_data()$number}.R"
+      )
+    } else if (plot_data()$tool == "Python") {
+      glue::glue(
+        "https://github.com/nrennie/30DayChartChallenge/tree/main/{plot_data()$year}/scripts/day_{plot_data()$number}.py"
+      )
+
+    } else if (plot_data()$tool == "ObservableJS") {
+      glue::glue(
+        "https://github.com/nrennie/30DayChartChallenge/tree/main/{plot_data()$year}/scripts/day_{plot_data()$number}/index.qmd"
+      )
+    } else {
+      NA
+    }
+  })
+
+  output$code_link <- shiny::renderText({
+    if (!is.na(code_path())) {
+      glue::glue(
+        '<b>Code is available at</b>: <a href="{code_path()}"  target="_blank">{code_path()}</a>.'
+      )
+    } else {
+      "Code is not available for this visualisation."
+    }
+  })
 
 }
 
