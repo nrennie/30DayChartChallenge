60605f2661cc23ca765c5072a7ec1024497415a5
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-03-19 18:25:51 +0000

    make 2025 consistent file structure

--- 2025/scripts/save_images.R
+++ 2025/scripts/save_images.R
@@ -1,15 +1,15 @@
 # Day 1 - Fractions -------------------------------------------------------
 
-quarto::quarto_render("2025/code/01_fractions/index.qmd")
+quarto::quarto_render("2025/scripts/day_01/index.qmd")
 httpuv::runStaticServer(
-  dir = "2025/code/01_fractions/",
+  dir = "2025/scripts/day_01/",
   port = 4000,
   background = TRUE,
   browse = FALSE
 )
 webshot2::webshot(
   url = "http://127.0.0.1:4000/",
-  file = "2025/viz/01_fractions.png",
+  file = "2025/viz/day_01.png",
   selector = ".cell-output.cell-output-display"
 )
 httpuv::stopAllServers()
