a36fc973d5ff05a4b5107d22f281bf9aa7a92cad
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-04-03 11:22:34 +0100

    remove empty border

--- 2025/scripts/day_11.py
+++ 2025/scripts/day_11.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import plotnine as gg
 import matplotlib.pyplot as plt
 
@@ -10,19 +9,15 @@ income = pd.read_csv("2025/data/income-share-of-the-richest-1.csv")
 plot_data = income[(income["Country"] == "World") & (income["Year"] >= 1980)].rename(columns={"Income share of the richest 1% (before tax) (World Inequality Database)": "Income"})
 plot_data['y'] = 1
 
-# Colours
-bg_col = "#FAFAFA"
-text_col = "#000000"
-
 # Plot
 g = (gg.ggplot()
     + gg.geom_tile(gg.aes(x="Year", y="y", fill="Income"), data=plot_data)
     + gg.scale_fill_continuous(cmap_name="bwr")
     + gg.theme_void()
+    + gg.coord_cartesian(expand=False)
     + gg.theme(
         legend_position="none",
         plot_margin=0,
-        plot_caption=gg.element_text(color=text_col, ha='left'),
         figure_size = (8, 4)
     )
 )
